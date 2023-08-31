cc22b90bf869623d4cd1636362d7d1a71cdbffbd
MavikBow <mrmavikd@gmail.com>
2023-08-31 20:15:57 +0300

    added the icon0 and background images

--- src/collision_lookup.c
+++ src/collision_lookup.c
@@ -32,15 +32,15 @@ int bottom_collision_1(float x, float y) //0 - no collision; 1 - yes collision
 	}
 	else
 	{
-		if((y >= 5 * SCALE && y <= 6 * SCALE) && (x > 13.5 * SCALE && x < 17.5 * SCALE))//square 1
+		if((y > 5 * SCALE && y <= 6 * SCALE) && (x > 13.5 * SCALE && x < 17.5 * SCALE))//square 1
 		{
 			return 1;
 		}
-		else if((y >= 6 * SCALE && y <= 7 * SCALE) && (x > 18.5 * SCALE && x < 22.5 * SCALE))//square 2
+		else if((y > 6 * SCALE && y <= 7 * SCALE) && (x > 18.5 * SCALE && x < 22.5 * SCALE))//square 2
 		{
 			return 1;
 		}
-		else if ((y >= 7 * SCALE && y <= 8 * SCALE) && (x > 24.5 * SCALE))//square 3
+		else if ((y > 7 * SCALE && y <= 8 * SCALE) && (x > 24.5 * SCALE))//square 3
 		{
 			return 1;
 		}
